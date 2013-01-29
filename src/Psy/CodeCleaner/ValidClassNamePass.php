40e85f4e55af51aee53d5bfb75c850820333bb12
Justin Hileman <justin@justinhileman.info>
2013-01-28 18:58:23 -0800

    Update ValidClassNamePass documentation.

--- src/Psy/CodeCleaner/ValidClassNamePass.php
+++ src/Psy/CodeCleaner/ValidClassNamePass.php
@@ -13,6 +13,7 @@ namespace Psy\CodeCleaner;
 
 use PHPParser_Node_Expr as Expression;
 use PHPParser_Node_Expr_New as NewExpression;
+use PHPParser_Node_Stmt as Statement;
 use PHPParser_Node_Stmt_Class as ClassStatement;
 use PHPParser_Node_Stmt_Interface as InterfaceStatement;
 use PHPParser_Node_Stmt_Trait as TraitStatement;
@@ -41,10 +42,13 @@ class ValidClassNamePass extends NamespaceAwarePass
     }
 
     /**
-     * Validate that classes exist.
+     * Validate class, interface and trait statements, and `new` expressions.
      *
-     * @throws FatalErrorException if a class is redefined.
-     * @throws FatalErrorException if the class name is a string (not an expression) and is not defined.
+     * @throws FatalErrorException if a class, interface or trait is referenced which does not exist.
+     * @throws FatalErrorException if a class extends something that is not a class.
+     * @throws FatalErrorException if a class implements something that is not an interface.
+     * @throws FatalErrorException if an interface extends something that is not an interface.
+     * @throws FatalErrorException if a class, interface or trait redefines an existing class, interface or trait name.
      *
      * @param mixed &$stmt
      */
@@ -59,11 +63,16 @@ class ValidClassNamePass extends NamespaceAwarePass
         } elseif ($stmt instanceof TraitStatement) {
             $this->validateTraitStatement($stmt);
         } elseif ($stmt instanceof NewExpression) {
-            $this->validateNewStatement($stmt);
+            $this->validateNewExpression($stmt);
         }
     }
 
-    protected function validateClassStatement($stmt)
+    /**
+     * Validate a class definition statment.
+     *
+     * @param ClassStatement $stmt
+     */
+    protected function validateClassStatement(ClassStatement $stmt)
     {
         $this->ensureCanDefine($stmt);
         if (isset($stmt->extends)) {
@@ -72,18 +81,33 @@ class ValidClassNamePass extends NamespaceAwarePass
         $this->ensureInterfacesExist($stmt->implements, $stmt);
     }
 
-    protected function validateInterfaceStatement($stmt)
+    /**
+     * Validate an interface definition statment.
+     *
+     * @param InterfaceStatement $stmt
+     */
+    protected function validateInterfaceStatement(InterfaceStatement $stmt)
     {
         $this->ensureCanDefine($stmt);
         $this->ensureInterfacesExist($stmt->extends, $stmt);
     }
 
-    protected function validateTraitStatement($stmt)
+    /**
+     * Validate a trait definition statment.
+     *
+     * @param TraitStatement $stmt
+     */
+    protected function validateTraitStatement(TraitStatement $stmt)
     {
         $this->ensureCanDefine($stmt);
     }
 
-    protected function validateNewStatement($stmt)
+    /**
+     * Validate a `new` expression.
+     *
+     * @param NewExpression $stmt
+     */
+    protected function validateNewExpression(NewExpression $stmt)
     {
         // if class name is an expression, give it a pass for now
         if (!$stmt->class instanceof Expression) {
@@ -91,7 +115,12 @@ class ValidClassNamePass extends NamespaceAwarePass
         }
     }
 
-    protected function ensureCanDefine($stmt)
+    /**
+     * Ensure that no class, interface or trait name collides with a new definition.
+     *
+     * @param  Statement $stmt
+     */
+    protected function ensureCanDefine(Statement $stmt)
     {
         $name = $this->getFullyQualifiedName($stmt->name);
 
@@ -114,7 +143,12 @@ class ValidClassNamePass extends NamespaceAwarePass
         $this->currentScope[$name] = $this->getScopeType($stmt);
     }
 
-
+    /**
+     * Ensure that a referenced class exists.
+     *
+     * @param  string    $name
+     * @param  Statement $stmt
+     */
     protected function ensureClassExists($name, $stmt)
     {
         if (!$this->classExists($name)) {
@@ -122,6 +156,12 @@ class ValidClassNamePass extends NamespaceAwarePass
         }
     }
 
+    /**
+     * Ensure that a referenced interface exists.
+     *
+     * @param  string    $name
+     * @param  Statement $stmt
+     */
     protected function ensureInterfacesExist($interfaces, $stmt)
     {
         foreach ($interfaces as $interface) {
@@ -132,7 +172,14 @@ class ValidClassNamePass extends NamespaceAwarePass
         }
     }
 
-    protected function getScopeType($stmt)
+    /**
+     * Get a symbol type key for storing in the scope name cache.
+     *
+     * @param  Statement $stmt [description]
+     *
+     * @return string
+     */
+    protected function getScopeType(Statement $stmt)
     {
         if ($stmt instanceof ClassStatement) {
             return self::CLASS_TYPE;
@@ -143,21 +190,49 @@ class ValidClassNamePass extends NamespaceAwarePass
         }
     }
 
+    /**
+     * Check whether a class exists, or has been defined in the current code snippet.
+     *
+     * @param  string $name
+     *
+     * @return boolean
+     */
     protected function classExists($name)
     {
         return class_exists($name) || $this->findInScope($name) == self::CLASS_TYPE;
     }
 
+    /**
+     * Check whether an interface exists, or has been defined in the current code snippet.
+     *
+     * @param  string $name
+     *
+     * @return boolean
+     */
     protected function interfaceExists($name)
     {
         return interface_exists($name) || $this->findInScope($name) == self::INTERFACE_TYPE;
     }
 
+    /**
+     * Check whether a trait exists, or has been defined in the current code snippet.
+     *
+     * @param  string $name
+     *
+     * @return boolean
+     */
     protected function traitExists($name)
     {
         return $this->checkTraits && (trait_exists($name) || $this->findInScope($name) == self::TRAIT_TYPE);
     }
 
+    /**
+     * Find a symbol in the current code snippet scope.
+     *
+     * @param  string $name
+     *
+     * @return string
+     */
     protected function findInScope($name)
     {
         if (isset($this->currentScope[$name])) {
@@ -165,6 +240,14 @@ class ValidClassNamePass extends NamespaceAwarePass
         }
     }
 
+    /**
+     * Error creation factory
+     *
+     * @param  string    $msg
+     * @param  Statement $stmt
+     *
+     * @return FatalErrorException
+     */
     protected function createError($msg, $stmt)
     {
         return new FatalErrorException($msg, 0, 1, null, $stmt->getLine());
