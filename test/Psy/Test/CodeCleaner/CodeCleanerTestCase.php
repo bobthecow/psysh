e647777aaecee9ae2eabc5fb2e6ef9a5fa0eb82e
Justin Hileman <justin@justinhileman.info>
2013-03-19 21:52:22 -0700

    Add CodeCleanerTestCase
    ...so I don't have to manually type out parse trees anymore.

---
+++ test/Psy/Test/CodeCleaner/CodeCleanerTestCase.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Psy\Test\CodeCleaner;
+
+use PHPParser_Lexer as Lexer;
+use PHPParser_Parser as Parser;
+use PHPParser_PrettyPrinter_Zend as Printer;
+use Psy\Exception\ParseErrorException;
+
+class CodeCleanerTestCase extends \PHPUnit_Framework_TestCase
+{
+    private $parser;
+    private $printer;
+
+    protected function parse($code, $prefix = '<?php ')
+    {
+        $code = $prefix.$code;
+        try {
+            return $this->getParser()->parse($code);
+        } catch (\PHPParser_Error $e) {
+            if ($e->getRawMessage() !== "Unexpected token EOF") {
+                throw ParseErrorException::fromParseError($e);
+            }
+
+            try {
+                // Unexpected EOF, try again with an implicit semicolon
+                return $this->getParser()->parse($code.';');
+            } catch (\PHPParser_Error $e) {
+                return false;
+            }
+        }
+    }
+
+    protected function prettyPrint(array $stmts)
+    {
+        return $this->getPrinter()->prettyPrint($stmts);
+    }
+
+    private function getParser()
+    {
+        if (!isset($this->parser)) {
+            $this->parser = new Parser(new Lexer);
+        }
+
+        return $this->parser;
+    }
+
+    private function getPrinter()
+    {
+        if (!isset($this->printer)) {
+            $this->printer = new Printer;
+        }
+
+        return $this->printer;
+    }
+}
