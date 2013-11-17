62d82e952e7f015ffc91e5601049a03b992281f5
Justin Hileman <justin@justinhileman.info>
2013-11-16 19:43:37 -0800

    Add missing copyright headers to codecleaner tests

--- test/Psy/Test/CodeCleaner/FunctionReturnInWriteContextPassTest.php
+++ test/Psy/Test/CodeCleaner/FunctionReturnInWriteContextPassTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Psy Shell
+ *
+ * (c) 2013 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Psy\Test\CodeCleaner;
 
 use PHPParser_NodeTraverser as NodeTraverser;
--- test/fixtures/rc.php
+++ test/fixtures/rc.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Psy Shell
+ *
+ * (c) 2013 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $config->setTempDir(sys_get_temp_dir().'/psysh_test/withconfig/temp');
 
 return array(
