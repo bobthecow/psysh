e6bd6b306f5e6c86725b9084c61baaa0120a1a15
Justin Hileman <justin@justinhileman.info>
2014-01-03 09:07:38 -0800

    Add code blocks as a single history entry.
    This has the awesome side effect of hoisting commands used inside code block declarations so that the code block can be replayed more effectively.

--- src/Psy/Shell.php
+++ src/Psy/Shell.php
@@ -263,9 +263,8 @@ class Shell extends Application
                 continue;
             }
 
-            $this->readline->addHistory($input);
-
             if ($this->hasCommand($input)) {
+                $this->readline->addHistory($input);
                 $this->runCommand($input);
                 continue;
             }
@@ -468,6 +467,7 @@ class Shell extends Application
     {
         if ($this->hasValidCode()) {
             $code = $this->code;
+            $this->readline->addHistory($code);
             $this->resetCodeBuffer();
 
             return $code;
