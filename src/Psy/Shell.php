535444c86f725b7dfd09fea6055e000083711454
Justin Hileman <justin@justinhileman.info>
2014-01-03 21:37:29 -0800

    Revert "Add code blocks as a single history entry."
    We'll bring it back, because it's a good idea, but the current
    implementation doesn't play nice with codecleaners... We're a bit at
    odds because we need to add the raw code block to the history, but
    execute the cleaned code block.
    
    This reverts commit e6bd6b306f5e6c86725b9084c61baaa0120a1a15.

--- src/Psy/Shell.php
+++ src/Psy/Shell.php
@@ -263,8 +263,9 @@ class Shell extends Application
                 continue;
             }
 
+            $this->readline->addHistory($input);
+
             if ($this->hasCommand($input)) {
-                $this->readline->addHistory($input);
                 $this->runCommand($input);
                 continue;
             }
@@ -467,7 +468,6 @@ class Shell extends Application
     {
         if ($this->hasValidCode()) {
             $code = $this->code;
-            $this->readline->addHistory($code);
             $this->resetCodeBuffer();
 
             return $code;
