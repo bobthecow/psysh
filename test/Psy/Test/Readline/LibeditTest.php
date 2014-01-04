234bf2c8cedc2725fd5c440735e774ada4a56d50
Justin Hileman <justin@justinhileman.info>
2014-01-04 07:33:03 -0800

    Revert "We shouldn’t clear history, since we’re starting a new history file."
    Yeah. That was a brain fart. We absolutely should.
    
    This reverts commit 939eec2127dd22534ccabc81d9411ad870176c4e.

--- test/Psy/Test/Readline/LibeditTest.php
+++ test/Psy/Test/Readline/LibeditTest.php
@@ -28,6 +28,7 @@ class LibeditTest extends \PHPUnit_Framework_TestCase
             $this->markTestSkipped('Missing unvis library');
         }
 
+        readline_clear_history();
         $this->historyFile = tempnam(sys_get_temp_dir().'/psysh/test/', 'history');
         $this->readline    = new Libedit($this->historyFile);
     }
