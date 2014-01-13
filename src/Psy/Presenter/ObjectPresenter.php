2599fe60308289966d55053f92fecc5a13f3e19f
James Shannon <james@jamesshannon.com>
2014-01-12 22:12:24 -0800

    Cleaned up unused variable

--- src/Psy/Presenter/ObjectPresenter.php
+++ src/Psy/Presenter/ObjectPresenter.php
@@ -103,7 +103,7 @@ class ObjectPresenter extends RecursivePresenter
     protected function getProperties($value, \ReflectionClass $class)
     {
         $deprecated = false;
-        $oldHandler = set_error_handler(function($errno, $errstr) use (&$deprecated) {
+        set_error_handler(function($errno, $errstr) use (&$deprecated) {
             if (in_array($errno, array(E_DEPRECATED, E_USER_DEPRECATED))) {
                 $deprecated = true;
             } else {
