ba519e935043e77a0465ab8983a4aa5793d97d64
Justin Hileman <justin@justinhileman.info>
2014-03-11 12:37:00 -0700

    That should be lower case

--- src/Psy/Compiler.php
+++ src/Psy/Compiler.php
@@ -151,7 +151,7 @@ EOS;
 
         $content = file_get_contents(__DIR__.'/../../bin/psysh');
         $content = preg_replace('{/\* <<<.*?>>> \*/}sm', $autoload, $content);
-        $content .= "__HALT_COMPILER();";
+        $content .= "__halt_compiler();";
 
         return $content;
     }
