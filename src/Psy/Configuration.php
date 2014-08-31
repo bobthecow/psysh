55e8f5db0a988e9842fef7ae78d61dc9b0765b5d
Justin Hileman <justin@justinhileman.info>
2014-08-31 10:25:41 -0700

    Support PSYSH_CONFIG to specify config file.
    Calling psysh with `--config` wins, of course.
    
    See #84

--- src/Psy/Configuration.php
+++ src/Psy/Configuration.php
@@ -71,6 +71,8 @@ class Configuration
         // explicit configFile option
         if (isset($config['configFile'])) {
             $this->configFile = $config['configFile'];
+        } elseif ($configFile = getenv('PSYSH_CONFIG')) {
+            $this->configFile = $configFile;
         }
 
         // legacy baseDir option
