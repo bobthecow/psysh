1eb2d41607715e4feb9f4f62f971365aebfba311
Justin Hileman <justin@justinhileman.info>
2014-09-09 22:21:26 -0700

    Version bump v0.2.0

--- src/Psy/Shell.php
+++ src/Psy/Shell.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Input\ArgvInput;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.1.12';
+    const VERSION = 'v0.2.0';
 
     const PROMPT      = '>>> ';
     const BUFF_PROMPT = '... ';
