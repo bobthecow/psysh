fc6fff49a758cb942ed5bc2d4c134ef9663677bc
Justin Hileman <justin@justinhileman.info>
2014-12-09 07:23:06 -0800

    Add test coverage for LegacyEmptyPass.

---
+++ test/Psy/Test/CodeCleaner/LegacyEmptyPassTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of Psy Shell
+ *
+ * (c) 2012-2014 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Psy\Test\CodeCleaner;
+
+use Psy\CodeCleaner\LegacyEmptyPass;
+
+class LegacyEmptyPassTest extends CodeCleanerTestCase
+{
+    public function setUp()
+    {
+        $this->setPass(new LegacyEmptyPass());
+    }
+
+    /**
+     * @dataProvider invalidStatements
+     * @expectedException \Psy\Exception\FatalErrorException
+     */
+    public function testProcessInvalidStatement($code)
+    {
+        $stmts = $this->parse($code);
+        $this->traverser->traverse($stmts);
+    }
+
+    public function invalidStatements()
+    {
+        return array(
+            array('empty()'),
+            array('empty(null)'),
+            array('empty(PHP_EOL)'),
+            array('empty("wat")'),
+            array('empty(1.1)'),
+        );
+    }
+
+    /**
+     * @dataProvider validStatements
+     */
+    public function testProcessValidStatement($code)
+    {
+        $stmts = $this->parse($code);
+        $this->traverser->traverse($stmts);
+    }
+
+    public function validStatements()
+    {
+        $data = array(
+            array('empty($foo)'),
+            array('empty(Bar::$baz)'),
+        );
+
+        return $data;
+    }
+}
