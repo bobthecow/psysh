33030dbc4f2902748b1c51643e569dc11f84713b
Justin Hileman <justin@justinhileman.info>
2015-03-13 13:09:46 -0700

    Include resource metadata when dumping resources.

--- src/Psy/Presenter/ResourcePresenter.php
+++ src/Psy/Presenter/ResourcePresenter.php
@@ -14,7 +14,7 @@ namespace Psy\Presenter;
 /**
  * A resource Presenter.
  */
-class ResourcePresenter implements Presenter
+class ResourcePresenter extends RecursivePresenter
 {
     const FMT = '<resource>\\<%s <strong>resource #%s</strong>></resource>';
 
@@ -59,8 +59,57 @@ class ResourcePresenter implements Presenter
      *
      * @return string
      */
-    public function present($value, $depth = null, $options = 0)
+    public function presentValue($value, $depth = null, $options = 0)
     {
-        return $this->presentRef($value);
+        if ($depth === 0 || !($options & Presenter::VERBOSE)) {
+            return $this->presentRef($value);
+        }
+
+        return sprintf('%s %s', $this->presentRef($value), $this->formatMetadata($value));
+    }
+
+    /**
+     * Format resource metadata.
+     *
+     * @param resource $value
+     *
+     * @return string
+     */
+    protected function formatMetadata($value)
+    {
+        $props = array();
+
+        switch (get_resource_type($value)) {
+            case 'stream':
+                $props = stream_get_meta_data($value);
+                break;
+
+            case 'curl':
+                $props = curl_getinfo($value);
+                break;
+
+            case 'xml':
+                $props = array(
+                    'current_byte_index'    => xml_get_current_byte_index($value),
+                    'current_column_number' => xml_get_current_column_number($value),
+                    'current_line_number'   => xml_get_current_line_number($value),
+                    'error_code'            => xml_get_error_code($value),
+                );
+                break;
+        }
+
+        if (empty($props)) {
+            return '{}';
+        }
+
+        $formatted = array();
+        foreach ($props as $name => $value) {
+            $formatted[] = sprintf('%s: %s', $name, $this->indentValue($this->presentSubValue($value)));
+        }
+
+        $template = sprintf('{%s%s%%s%s}', PHP_EOL, self::INDENT, PHP_EOL);
+        $glue     = sprintf(',%s%s', PHP_EOL, self::INDENT);
+
+        return sprintf($template, implode($glue, $formatted));
     }
 }
