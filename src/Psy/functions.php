de47b2023fbbbaeeeac0df97adec848b78e6adb8
Justin Hileman <justin@justinhileman.info>
2015-03-13 12:07:09 -0700

    Fix warning about undefined $this in Psy\sh()

--- src/Psy/functions.php
+++ src/Psy/functions.php
@@ -12,6 +12,6 @@ if (!function_exists('Psy\sh')) {
      */
     function sh()
     {
-        return 'extract(\Psy\Shell::debug(get_defined_vars(), $this ?: null));';
+        return 'extract(\Psy\Shell::debug(get_defined_vars(), isset($this) ? $this : null));';
     }
 }
