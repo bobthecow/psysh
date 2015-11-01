990c0fd5419bc7b0230d25ae8bdf47af6c17ed01
Alexey Shockov <alexey@shockov.com>
2015-11-01 12:22:07 +0300

    Tests refactoring to support PHP pareser 2.0

--- test/Psy/Test/CodeCleaner/CodeCleanerTestCase.php
+++ test/Psy/Test/CodeCleaner/CodeCleanerTestCase.php
@@ -11,10 +11,10 @@
 
 namespace Psy\Test\CodeCleaner;
 
-use PHPParser_Lexer as Lexer;
-use PHPParser_NodeTraverser as NodeTraverser;
-use PHPParser_Parser as Parser;
-use PHPParser_PrettyPrinter_Default as Printer;
+use PhpParser\Lexer;
+use PhpParser\NodeTraverser;
+use PhpParser\Parser;
+use PhpParser\PrettyPrinter\Standard as Printer;
 use Psy\CodeCleaner\CodeCleanerPass;
 use Psy\Exception\ParseErrorException;
 
@@ -39,7 +39,7 @@ class CodeCleanerTestCase extends \PHPUnit_Framework_TestCase
         $code = $prefix . $code;
         try {
             return $this->getParser()->parse($code);
-        } catch (\PHPParser_Error $e) {
+        } catch (\PhpParser\Error $e) {
             if (!$this->parseErrorIsEOF($e)) {
                 throw ParseErrorException::fromParseError($e);
             }
@@ -47,7 +47,7 @@ class CodeCleanerTestCase extends \PHPUnit_Framework_TestCase
             try {
                 // Unexpected EOF, try again with an implicit semicolon
                 return $this->getParser()->parse($code . ';');
-            } catch (\PHPParser_Error $e) {
+            } catch (\PhpParser\Error $e) {
                 return false;
             }
         }
@@ -88,7 +88,7 @@ class CodeCleanerTestCase extends \PHPUnit_Framework_TestCase
         return $this->printer;
     }
 
-    private function parseErrorIsEOF(\PHPParser_Error $e)
+    private function parseErrorIsEOF(\PhpParser\Error $e)
     {
         $msg = $e->getRawMessage();
 
