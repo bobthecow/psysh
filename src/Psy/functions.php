08403cff2f114eb19b6a2de65ae779061a842930
Justin Hileman <justin@justinhileman.info>
2015-11-06 06:00:41 -0800

    Update copyright docblocks.

--- src/Psy/functions.php
+++ src/Psy/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Psy Shell
+ *
+ * (c) 2012-2015 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Psy;
 
 use Symfony\Component\Console\Input\ArgvInput;
