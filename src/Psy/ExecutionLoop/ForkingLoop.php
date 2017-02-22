0fc522da336d0ccdff808882e97964b50169fd3a
Damian Lee <damiankloip@gmail.com>
2017-02-22 11:41:08 +0000

    Catch PHP7 \Errors in ForkingLoop::serializeReturn()

--- src/Psy/ExecutionLoop/ForkingLoop.php
+++ src/Psy/ExecutionLoop/ForkingLoop.php
@@ -167,6 +167,9 @@ class ForkingLoop extends Loop
             } catch (\Exception $e) {
                 // we'll just ignore this one...
             }
+            catch (\Error $e) {
+                // and this one too...
+            }
         }
 
         return @serialize($serializable);
