80f6a70e0f426b69907a6314be1514db7edbef13
USAMI Kenta <tadsan@zonu.me>
2017-05-27 12:10:04 +0900

    Use variable instead of string literal
    This is a compatibility issue, but since it is not an essential
    problem, ignore it by replacing the string with a variable.

--- test/Psy/Test/CodeCleaner/ExitPassTest.php
+++ test/Psy/Test/CodeCleaner/ExitPassTest.php
@@ -50,7 +50,7 @@ class ExitPassTest extends CodeCleanerTestCase
             array('1 and exit();', "1 and {$this->expectedExceptionString};"),
             array('foo() or die', "foo() or {$this->expectedExceptionString};"),
             array('exit and 1;', "{$this->expectedExceptionString} and 1;"),
-            array('if (exit) { echo "wat"; }', "if ({$this->expectedExceptionString}) {\n    echo \"wat\";\n}"),
+            array('if (exit) { echo $wat; }', "if ({$this->expectedExceptionString}) {\n    echo \$wat;\n}"),
             array('exit or die;', "{$this->expectedExceptionString} or {$this->expectedExceptionString};"),
             array('switch (die) { }', "switch ({$this->expectedExceptionString}) {\n}"),
             array('for ($i = 1; $i < 10; die) {}', "for (\$i = 1; \$i < 10; {$this->expectedExceptionString}) {\n}"),
