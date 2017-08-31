f3f791171eeda09cea9015130074ae7ef5c97547
Tim Bazuin <krageon@gmail.com>
2017-08-31 14:36:23 +0200

    added support for giving a variable as an argument to edit, cleaned up the code in EditCommand

--- src/Psy/Command/EditCommand.php
+++ src/Psy/Command/EditCommand.php
@@ -2,20 +2,31 @@
 
 namespace Psy\Command;
 
+use Psy\Context;
+use Psy\ContextAware;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class EditCommand extends Command
+class EditCommand extends Command implements ContextAware
 {
+    /**
+     * @var string
+     */
     private $runtimeDir = '';
+
+    /**
+     * @var Context
+     */
+    private $context;
 
     /**
      * Constructor.
      *
-     * @param string      $runtimeDir The directory to use for temporary files
-     * @param string|null $name       The name of the command; passing null means it must be set in configure()
+     * @param string $runtimeDir The directory to use for temporary files
+     * @param string|null $name The name of the command; passing null means it must be set in configure()
      *
      * @throws \Symfony\Component\Console\Exception\LogicException When the command name is empty
      */
@@ -53,37 +64,107 @@ class EditCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($input->getOption('exec') && $input->getOption('no-exec')) {
+        if ($input->getOption('exec') &&
+            $input->getOption('no-exec')) {
             throw new \InvalidArgumentException('The --exec and --no-exec flags are mutually exclusive.');
         }
 
-        $filePath = $input->getArgument('file');
-        $execute = $filePath === null;
+        $filePath = $this->extractFilePath($input->getArgument('file'));
+
+        $execute = $this->shouldExecuteFile(
+            $input->getOption('exec'),
+            $input->getOption('no-exec'),
+            $filePath
+        );
+
+        $shouldRemoveFile = false;
 
         if ($filePath === null) {
             $filePath = tempnam($this->runtimeDir, 'psysh-edit-command');
+            $shouldRemoveFile = true;
         }
 
-        if ($input->getOption('exec')) {
-            $execute = true;
-        } elseif ($input->getOption('no-exec')) {
-            $execute = false;
+        $editedContent = $this->editFile($filePath, $shouldRemoveFile);
+
+        if ($execute) {
+            $this->getApplication()->addInput($editedContent);
+        }
+    }
+
+    /**
+     * @param bool $execOption
+     * @param bool $noExecOption
+     * @param string|null $filePath
+     *
+     * @return bool
+     */
+    private function shouldExecuteFile($execOption, $noExecOption, $filePath)
+    {
+        if ($execOption) {
+            return true;
         }
 
+        if ($noExecOption) {
+            return false;
+        }
+
+        // By default, code that is edited is executed if there was no given input file path
+        return $filePath === null;
+    }
+
+    /**
+     * @param string|null $fileArgument
+     * @return string|null The file path to edit, null if the input was null, or the value of the referenced variable
+     *
+     * @throws \InvalidArgumentException If the variable is not found in the current context
+     */
+    private function extractFilePath($fileArgument)
+    {
+        // If the file argument was a variable, get it from the context
+        if ($fileArgument !== null &&
+            strlen($fileArgument) > 0 &&
+            $fileArgument[0] === '$') {
+            $fileArgument = $this->context->get(preg_replace('/^\$/', '', $fileArgument));
+        }
+
+        return $fileArgument;
+    }
+
+    /**
+     * @param string $filePath
+     * @param string $shouldRemoveFile
+     * @return string
+     *
+     * @throws \UnexpectedValueException if file_get_contents on $filePath returns false instead of a string
+     */
+    private function editFile($filePath, $shouldRemoveFile)
+    {
         $escapedFilePath = escapeshellarg($filePath);
 
         $pipes = array();
         $proc = proc_open((getenv('EDITOR') ?: 'nano') . " {$escapedFilePath}", array(STDIN, STDOUT, STDERR), $pipes);
         proc_close($proc);
 
-        $editedContent = file_get_contents($filePath);
+        $editedContent = @file_get_contents($filePath);
 
-        if ($input->getArgument('file') === null) {
+        if ($shouldRemoveFile) {
             @unlink($filePath);
         }
 
-        if ($execute) {
-            $this->getApplication()->addInput($editedContent);
+        if ($editedContent === false) {
+            throw new \UnexpectedValueException("Reading {$filePath} returned false");
         }
+
+        return $editedContent;
+    }
+
+    /**
+     * Set the Context reference.
+     *
+     * @param Context $context
+     */
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
     }
 }
