0707a92317ca19206a6e3b2d0af404ee59047d06
Tim Bazuin <krageon@gmail.com>
2017-09-04 12:54:08 +0200

    applied styleci patch

--- src/Psy/TabCompletion/Matcher/AbstractDefaultParametersMatcher.php
+++ src/Psy/TabCompletion/Matcher/AbstractDefaultParametersMatcher.php
@@ -1,18 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tim
- * Date: 9/4/17
- * Time: 10:36 AM
- */
 
 namespace Psy\TabCompletion\Matcher;
-
 
 abstract class AbstractDefaultParametersMatcher extends AbstractContextAwareMatcher
 {
     /**
      * @param \ReflectionParameter[] $reflectionParameters
+     *
      * @return array
      */
     public function getDefaultParameterCompletion(array $reflectionParameters)
