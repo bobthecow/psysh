b7f499a7d253c0c023f20f05177495686c62a45d
Justin Hileman <justin@justinhileman.info>
2017-12-10 13:49:21 -0800

    Bump to v0.8.16

--- src/Psy/Shell.php
+++ src/Psy/Shell.php
@@ -44,7 +44,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.8.15';
+    const VERSION = 'v0.8.16';
 
     const PROMPT      = '>>> ';
     const BUFF_PROMPT = '... ';
