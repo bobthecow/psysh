c6106e000664ba350982aef55240b809fdba75c5
Justin Hileman <justin@justinhileman.info>
2018-01-15 10:18:50 -0800

    Add -n flag to timeit command.

--- src/Command/TimeitCommand.php
+++ src/Command/TimeitCommand.php
@@ -14,6 +14,7 @@ namespace Psy\Command;
 use Psy\Input\CodeArgument;
 use Psy\Shell;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -21,6 +22,9 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class TimeitCommand extends Command
 {
+    const RESULT_MSG     = '<info>Command took %.6f seconds to complete.</info>';
+    const AVG_RESULT_MSG = '<info>Command took %.6f seconds on average (%.6f median; %.6f total) to complete.</info>';
+
     /**
      * {@inheritdoc}
      */
@@ -29,6 +33,7 @@ class TimeitCommand extends Command
         $this
             ->setName('timeit')
             ->setDefinition([
+                new InputOption('num', 'n', InputOption::VALUE_REQUIRED, 'Number of iterations.'),
                 new CodeArgument('code', CodeArgument::REQUIRED, 'Code to execute.'),
             ])
             ->setDescription('Profiles with a timer.')
@@ -38,7 +43,7 @@ Time profiling for functions and commands.
 
 e.g.
 <return>>>> timeit sleep(1)</return>
-<return>>>> timeit $closure()</return>
+<return>>>> timeit -n1000 $closure()</return>
 HELP
             );
     }
@@ -49,13 +54,26 @@ HELP
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $code = $input->getArgument('code');
+        $num = $input->getOption('num') ?: 1;
         $shell = $this->getApplication();
 
-        $start = microtime(true);
-        $_ = $shell->execute($code);
-        $end = microtime(true);
+        $times = [];
+        for ($i = 0; $i < $num; $i++) {
+            $start = microtime(true);
+            $_ = $shell->execute($code);
+            $times[] = microtime(true) - $start;
+        }
 
         $shell->writeReturnValue($_);
-        $output->writeln(sprintf('<info>Command took %.6f seconds to complete.</info>', $end - $start));
+
+        if ($num === 1) {
+            $output->writeln(sprintf(self::RESULT_MSG, $times[0]));
+        } else {
+            $total = array_sum($times);
+            rsort($times);
+            $median = $times[round($num / 2)];
+
+            $output->writeln(sprintf(self::AVG_RESULT_MSG, $total / $num, $median, $total));
+        }
     }
 }
