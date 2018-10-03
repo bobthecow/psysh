96e074249033e37754cd19bfa367b264115983c3
Justin Hileman <justin@justinhileman.info>
2018-10-03 01:27:37 -0700

    Only trim \r and \n from transient readline input.
    The current `rtrim` call would remove trailing whitespace in strings or heredocs split between two lines when the code buffer is left open due to incomplete statements in input.

--- src/Readline/Transient.php
+++ src/Readline/Transient.php
@@ -106,7 +106,7 @@ class Transient implements Readline
     {
         echo $prompt;
 
-        return \rtrim(\fgets($this->getStdin()));
+        return \rtrim(\fgets($this->getStdin()), "\n\r");
     }
 
     /**
