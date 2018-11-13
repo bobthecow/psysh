3e15947c4295996b495f037b64d65154c10692ce
Théo FIDRY <theo.fidry@gmail.com>
2018-11-13 22:43:07 +0100

    Finish

--- scoper.inc.php
+++ scoper.inc.php
@@ -5,4 +5,16 @@ return [
         'Psy\Shell',
         'Psy\bin',
     ],
+    'patchers' => [
+        function (string $filePath, string $prefix, string $contents): string {
+            return 'src/ConfigPaths.php' === $filePath
+                ? str_replace(
+                    "'Psy\\\\Exception\\\\ErrorException'",
+                    "'$prefix\\\\Psy\\\\Exception\\\\ErrorException'",
+                    $contents
+                )
+                : $contents
+            ;
+        },
+    ],
 ];
