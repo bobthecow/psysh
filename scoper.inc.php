43fdb75b678ffb5bf9407b5fb9639d106be2097c
Théo FIDRY <theo.fidry@gmail.com>
2018-11-16 14:45:06 +0100

    Update scoper config

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,19 +2,46 @@
 
 return [
     'whitelist' => [
-        'Psy\Shell',
-        'Psy\bin',
+        'Psy\*',
     ],
+
     'patchers' => [
+        // Hoa patches
         function (string $filePath, string $prefix, string $contents): string {
-            return 'src/ConfigPaths.php' === $filePath
-                ? str_replace(
-                    "'Psy\\\\Exception\\\\ErrorException'",
-                    "'$prefix\\\\Psy\\\\Exception\\\\ErrorException'",
-                    $contents
-                )
-                : $contents
-            ;
+            if ('vendor/hoa/stream/Stream.php' !== $filePath) {
+                return $contents;
+            }
+            return preg_replace(
+                '/Hoa\\\\Consistency::registerShutdownFunction\(xcallable\(\'(.*)\'\)\)/',
+                sprintf(
+                    'Hoa\\Consistency::registerShutdownFunction(xcallable(\'%s$1\'))',
+                    $prefix.'\\\\\\\\'
+                ),
+                $contents
+            );
         },
-    ],
+        function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/hoa/consistency/Autoloader.php' !== $filePath) {
+                return $contents;
+            }
+            $contents = preg_replace(
+                '/(\$entityPrefix = \$entity;)/',
+                sprintf(
+                    '$entity = substr($entity, %d);$1',
+                    strlen($prefix) + 1
+                ),
+                $contents
+            );
+            $contents = preg_replace(
+                '/return \$this->runAutoloaderStack\((.*)\);/',
+                sprintf(
+                    'return $this->runAutoloaderStack(\'%s\'.\'%s\'.$1);',
+                    $prefix,
+                    '\\\\\\'
+                ),
+                $contents
+            );
+            return $contents;
+        },
+    ]
 ];
