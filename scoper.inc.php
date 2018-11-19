7e2d5a975f1668bbf0707ac0fb07e088bae5ad27
Théo FIDRY <theo.fidry@gmail.com>
2018-11-19 11:01:16 +0100

    Update scoper config

--- scoper.inc.php
+++ scoper.inc.php
@@ -3,11 +3,18 @@
 return [
     'whitelist' => [
         'Psy\*',
+
+        // Old Hoa global functions
+        'from',
+        'dnew',
+        'xcallable',
+        'curry',
+        'curry_ref',
     ],
 
     'patchers' => [
         // Hoa patches
-        function (string $filePath, string $prefix, string $contents): string {
+        static function (string $filePath, string $prefix, string $contents): string {
             if ('vendor/hoa/stream/Stream.php' !== $filePath) {
                 return $contents;
             }
@@ -21,7 +28,7 @@ return [
                 $contents
             );
         },
-        function (string $filePath, string $prefix, string $contents): string {
+        static function (string $filePath, string $prefix, string $contents): string {
             if ('vendor/hoa/consistency/Autoloader.php' !== $filePath) {
                 return $contents;
             }
@@ -44,6 +51,150 @@ return [
             );
 
             return $contents;
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if (!in_array($filePath, ['vendor/hoa/console/Mouse.php', 'vendor/hoa/console/Console.php', 'vendor/hoa/core/Consistency.php'], true)) {
+                return $contents;
+            }
+
+            return preg_replace(
+                '/\'(?:\\\\){0,2}(Hoa\\\\.+?)(::.+)\'/',
+                sprintf(
+                    '\'%s\\\\\\\$1$2\'',
+                    $prefix
+                ),
+                $contents
+            );
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/hoa/core/Consistency.php' === $filePath) {
+                return $contents;
+            }
+
+            return str_replace(
+                '$classname = \ltrim($classname, \'\\\\\');',
+                sprintf(
+                    '$classname = \substr(\ltrim($classname, \'\\\\\'), %d);',
+                    strlen($prefix) + 1
+                ),
+                $contents
+            );
+        },
+        // https://github.com/humbug/php-scoper/issues/294
+        // https://github.com/humbug/php-scoper/issues/286
+        static function (string $filePath, string $prefix, string $contents): string {
+            if (!in_array($filePath, ['src/Formatter/DocblockFormatter.php', 'src/Output/ShellOutput.php'], true)) {
+                return $contents;
+            }
+
+            return str_replace(
+                '\'Symfony\\\\Component\\\\Console\\\\Formatter\\\\OutputFormatter\'',
+                sprintf(
+                    '\'%s\\%s\'',
+                    $prefix,
+                    'Symfony\Component\Console\Formatter\OutputFormatter'
+                ),
+                $contents
+            );
+        },
+        // Symfony patches
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/symfony/debug/DebugClassLoader.php' !== $filePath) {
+                return $contents;
+            }
+
+            return preg_replace(
+                '/case \'(Symfony\\\\.+\\\\)\':/',
+                sprintf(
+                    'case \'%s\\\\\\\$1\':',
+                    $prefix
+                ),
+                $contents
+            );
+        },
+        // https://github.com/humbug/php-scoper/issues/286
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/symfony/var-dumper/Cloner/AbstractCloner.php' !== $filePath) {
+                return $contents;
+            }
+
+            return preg_replace(
+                '/\'(Symfony\\\\.+?)\'/',
+                sprintf(
+                    '\'%s\\\\\\\$1\'',
+                    $prefix
+                ),
+                $contents
+            );
+        },
+        // https://github.com/humbug/php-scoper/issues/286
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/symfony/debug/Exception/FlattenException.php' !== $filePath) {
+                return $contents;
+            }
+
+            return preg_replace(
+                '/\'(Symfony\\\\.+?)\'/',
+                sprintf(
+                    '\'%s\\\\\\\$1\'',
+                    $prefix
+                ),
+                $contents
+            );
+        },
+        // PHP-Parser patches
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/nikic/php-parser/lib/PhpParser/JsonDecoder.php' !== $filePath) {
+                return $contents;
+            }
+
+            return str_replace(
+                '\'PhpParser\\\\Node\\\\\'',
+                sprintf(
+                    '\'%s\\\\PhpParser\\\\Node\\\\\'',
+                    $prefix
+                ),
+                $contents
+            );
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/nikic/php-parser/lib/PhpParser/Unserializer/XML.php' !== $filePath) {
+                return $contents;
+            }
+
+            $contents = preg_replace(
+                '/\'(PhpParser\\\\.+(?:\\\\)?)\'/',
+                sprintf(
+                    '\'%s\\\\\\\$1\'',
+                    $prefix
+                ),
+                $contents
+            );
+
+            $contents = preg_replace(
+                '/\'(PhpParser\\\\\\\\\p{L}+)(?:\\\\\\\\)?\'/u',
+                sprintf(
+                    '\'%s\\\\\\\$1\'',
+                    $prefix
+                ),
+                $contents
+            );
+
+            return $contents;
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('vendor/nikic/php-parser/lib/PhpParser/Lexer.php' !== $filePath) {
+                return $contents;
+            }
+
+            return str_replace(
+                '\'PhpParser\\\\Parser\\\\Tokens::\'',
+                sprintf(
+                    '\'%s\\\\PhpParser\\\\Parser\\\\Tokens::\'',
+                    $prefix
+                ),
+                $contents
+            );
         },
     ],
 ];
