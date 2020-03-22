2a7589525ee6583eb6a06d23db2e791b37dd44b6
Justin Hileman <justin@justinhileman.info>
2020-03-21 22:04:00 -0700

    Deprecate ConsoleColorFactory and remove highlighter dependency.

--- src/ConsoleColorFactory.php
+++ src/ConsoleColorFactory.php
@@ -12,21 +12,18 @@
 namespace Psy;
 
 use JakubOnderka\PhpConsoleColor\ConsoleColor;
-use JakubOnderka\PhpConsoleHighlighter\Highlighter;
 
 /**
- * Builds `ConsoleColor` instances configured according to the given color mode.
+ * @deprecated Nothing should use this anymore
  */
 class ConsoleColorFactory
 {
-    private $colorMode;
-
     /**
      * @param string $colorMode
      */
     public function __construct($colorMode)
     {
-        $this->colorMode = $colorMode;
+        // Nothing to see here
     }
 
     /**
@@ -37,46 +34,6 @@ class ConsoleColorFactory
      */
     public function getConsoleColor()
     {
-        if ($this->colorMode === Configuration::COLOR_MODE_AUTO) {
-            return $this->getDefaultConsoleColor();
-        } elseif ($this->colorMode === Configuration::COLOR_MODE_FORCED) {
-            return $this->getForcedConsoleColor();
-        } elseif ($this->colorMode === Configuration::COLOR_MODE_DISABLED) {
-            return $this->getDisabledConsoleColor();
-        }
-    }
-
-    private function getDefaultConsoleColor()
-    {
-        $color = new ConsoleColor();
-        $color->addTheme(Highlighter::LINE_NUMBER, ['blue']);
-        $color->addTheme(Highlighter::TOKEN_KEYWORD, ['yellow']);
-        $color->addTheme(Highlighter::TOKEN_STRING, ['green']);
-        $color->addTheme(Highlighter::TOKEN_COMMENT, ['dark_gray']);
-
-        return $color;
-    }
-
-    private function getForcedConsoleColor()
-    {
-        $color = $this->getDefaultConsoleColor();
-        $color->setForceStyle(true);
-
-        return $color;
-    }
-
-    private function getDisabledConsoleColor()
-    {
-        $color = new ConsoleColor();
-
-        $color->addTheme(Highlighter::TOKEN_STRING, ['none']);
-        $color->addTheme(Highlighter::TOKEN_COMMENT, ['none']);
-        $color->addTheme(Highlighter::TOKEN_KEYWORD, ['none']);
-        $color->addTheme(Highlighter::TOKEN_DEFAULT, ['none']);
-        $color->addTheme(Highlighter::TOKEN_HTML, ['none']);
-        $color->addTheme(Highlighter::ACTUAL_LINE_MARK, ['none']);
-        $color->addTheme(Highlighter::LINE_NUMBER, ['none']);
-
-        return $color;
+        return new ConsoleColor(); // /shrug
     }
 }
