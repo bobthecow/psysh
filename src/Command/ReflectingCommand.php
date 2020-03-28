c23d4f09bf6894df7492442556e99bdee94f6687
Justin Hileman <justin@justinhileman.info>
2020-03-28 07:55:50 -0700

    Implicit sudo for reflecting commands

--- src/Command/ReflectingCommand.php
+++ src/Command/ReflectingCommand.php
@@ -11,14 +11,18 @@
 
 namespace Psy\Command;
 
+use PhpParser\NodeTraverser;
+use PhpParser\PrettyPrinter\Standard as Printer;
 use Psy\CodeCleaner\NoReturnValue;
 use Psy\Context;
 use Psy\ContextAware;
 use Psy\Exception\ErrorException;
 use Psy\Exception\RuntimeException;
 use Psy\Exception\UnexpectedTargetException;
+use Psy\ParserFactory;
 use Psy\Reflection\ReflectionClassConstant;
 use Psy\Reflection\ReflectionConstant_;
+use Psy\Sudo\SudoVisitor;
 use Psy\Util\Mirror;
 
 /**
@@ -37,6 +41,26 @@ abstract class ReflectingCommand extends Command implements ContextAware
      * @var Context
      */
     protected $context;
+
+    private $parser;
+    private $traverser;
+    private $printer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($name = null)
+    {
+        $parserFactory = new ParserFactory();
+        $this->parser = $parserFactory->createParser();
+
+        $this->traverser = new NodeTraverser();
+        $this->traverser->addVisitor(new SudoVisitor());
+
+        $this->printer = new Printer();
+
+        parent::__construct($name);
+    }
 
     /**
      * ContextAware interface.
@@ -170,7 +194,10 @@ abstract class ReflectingCommand extends Command implements ContextAware
     protected function resolveCode(string $code)
     {
         try {
-            $value = $this->getApplication()->execute($code, true);
+            // Add an implicit `sudo` to target resolution.
+            $nodes = $this->traverser->traverse($this->parse($code));
+            $sudoCode = $this->printer->prettyPrint($nodes);
+            $value = $this->getApplication()->execute($sudoCode, true);
         } catch (\Throwable $e) {
             // Swallow all exceptions?
         }
@@ -180,6 +207,29 @@ abstract class ReflectingCommand extends Command implements ContextAware
         }
 
         return $value;
+    }
+
+    /**
+     * Lex and parse a string of code into statements.
+     *
+     * @param string $code
+     *
+     * @return array Statements
+     */
+    private function parse($code)
+    {
+        $code = '<?php '.$code;
+
+        try {
+            return $this->parser->parse($code);
+        } catch (\PhpParser\Error $e) {
+            if (\strpos($e->getMessage(), 'unexpected EOF') === false) {
+                throw $e;
+            }
+
+            // If we got an unexpected EOF, let's try it again with a semicolon.
+            return $this->parser->parse($code.';');
+        }
     }
 
     /**
