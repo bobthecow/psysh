2c81b842760ce49964cb61341add376ed42e561d
Justin Hileman <justin@justinhileman.info>
2020-05-02 15:27:23 -0700

    Remove unused paths from phan config

--- .phan/config.php
+++ .phan/config.php
@@ -17,15 +17,9 @@ return [
     'directory_list' => [
         'src/',
         'vendor/dnoegel/php-xdg-base-dir/src/',
-        'vendor/doctrine/instantiator/src/',
         'vendor/hoa/console/',
-        'vendor/jakub-onderka/php-console-color/src/',
-        'vendor/jakub-onderka/php-console-highlighter/src/',
         'vendor/nikic/php-parser/lib/',
-        'vendor/phpdocumentor/reflection-docblock/',
         'vendor/symfony/console/',
-        'vendor/symfony/filesystem/',
-        'vendor/symfony/finder/',
         'vendor/symfony/var-dumper/',
     ],
 
