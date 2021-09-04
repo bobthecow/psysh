40476643917a46b7b440e25d1c56655ecbb64d0e
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2021-09-04 18:02:42 +0100

    Fixes for upcoming update to StyleCI Symfony preset

--- src/functions.php
+++ src/functions.php
@@ -331,12 +331,12 @@ if (!\function_exists('Psy\\bin')) {
     {
         return function () {
             if (!isset($_SERVER['PSYSH_IGNORE_ENV']) || !$_SERVER['PSYSH_IGNORE_ENV']) {
-                if (\defined('HHVM_VERSION_ID') && \HHVM_VERSION_ID < 31800) {
+                if (\defined('HHVM_VERSION_ID') && HHVM_VERSION_ID < 31800) {
                     \fwrite(\STDERR, 'HHVM 3.18 or higher is required. You can set the environment variable PSYSH_IGNORE_ENV=1 to override this restriction and proceed anyway.'.\PHP_EOL);
                     exit(1);
                 }
 
-                if (\defined('HHVM_VERSION_ID') && \HHVM_VERSION_ID > 39999) {
+                if (\defined('HHVM_VERSION_ID') && HHVM_VERSION_ID > 39999) {
                     \fwrite(\STDERR, 'HHVM 4 or higher is not supported. You can set the environment variable PSYSH_IGNORE_ENV=1 to override this restriction and proceed anyway.'.\PHP_EOL);
                     exit(1);
                 }
