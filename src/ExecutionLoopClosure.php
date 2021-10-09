665369198d51e3f1bb8bcf7cb54e9ec6e860fade
Justin Hileman <justin@justinhileman.info>
2021-10-09 08:38:42 -0400

    Code style tweak.

--- src/ExecutionLoopClosure.php
+++ src/ExecutionLoopClosure.php
@@ -33,7 +33,7 @@ class ExecutionLoopClosure extends ExecutionClosure
             // Restore execution scope variables
             \extract($__psysh__->getScopeVariables(false));
 
-            do {
+            while (true) {
                 $__psysh__->beforeLoop();
 
                 try {
@@ -96,7 +96,7 @@ class ExecutionLoopClosure extends ExecutionClosure
                 }
 
                 $__psysh__->afterLoop();
-            } while (true);
+            }
         });
     }
 }
