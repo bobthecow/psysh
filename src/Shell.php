c3ee5ffdf145f3ac6dedeefcbfee8f251dca6ebe
Justin Hileman <justin@justinhileman.info>
2021-10-10 09:35:51 -0400

    Bump to v0.10.9

--- src/Shell.php
+++ src/Shell.php
@@ -48,7 +48,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.10.8';
+    const VERSION = 'v0.10.9';
 
     const PROMPT = '>>> ';
     const BUFF_PROMPT = '... ';
