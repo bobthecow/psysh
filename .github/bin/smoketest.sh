fc696a811c6a9abca40ccbde1706dd3d23ee75fb
Justin Hileman <justin@justinhileman.info>
2021-11-23 08:52:36 -0500

    Add smoketest for `help` command.

--- .github/bin/smoketest.sh
+++ .github/bin/smoketest.sh
@@ -46,11 +46,29 @@ test_psy_info() {
   echo "Passed"
 }
 
+test_help_command() {
+  echo -n "  help:  "
+
+  output=$(echo "help" | build/$1/psysh 2>&1 | cat)
+  if [ $? != 0 ]; then
+    fail "$output"
+    return
+  fi
+
+  [[ "$output" = *"help"* ]] || fail "$output"
+  [[ "$output" = *"Show a list of commands."* ]] || fail "$output"
+  [[ "$output" = *"wtf"* ]] || fail "$output"
+  [[ "$output" = *"Show the backtrace of the most recent exception."* ]] || fail "$output"
+  [[ "$output" = *"exit"* ]] || fail "$output"
+  [[ "$output" = *"End the current session and return to caller."* ]] || fail "$output"
+}
+
 for build in ${build_versions[@]}; do
   echo "Testing $build phar"
 
   test_version $build
   test_psy_info $build
+  test_help_command $build
 
   echo
 done
