81a37822e62a0e0af3eb5823c3ca16cc4d39deb6
Justin Hileman <justin@justinhileman.info>
2022-01-03 09:10:40 -0500

    $year++

--- test/fixtures/empty.php
+++ test/fixtures/empty.php
@@ -3,10 +3,9 @@
 /*
  * This file is part of Psy Shell.
  *
- * (c) 2012-2020 Justin Hileman
+ * (c) 2012-2022 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 /* this space intentionally left blank */
