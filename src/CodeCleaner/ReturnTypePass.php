9b7593b2183942806c75ce3fdb871e797ea4cf94
Justin Hileman <justin@justinhileman.info>
2022-06-14 05:26:51 -0400

    Improve support for union return types in code cleaner
    Fixes #725

--- src/CodeCleaner/ReturnTypePass.php
+++ src/CodeCleaner/ReturnTypePass.php
@@ -18,6 +18,7 @@ use PhpParser\Node\Identifier;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\UnionType;
 use Psy\Exception\FatalErrorException;
 
 /**
@@ -105,6 +106,10 @@ class ReturnTypePass extends CodeCleanerPass
 
     private function typeName(Node $node): string
     {
+        if ($node instanceof UnionType) {
+            return \implode('|', \array_map([$this, 'typeName'], $node->types));
+        }
+
         if ($node instanceof NullableType) {
             return \strtolower($node->type->name);
         }
