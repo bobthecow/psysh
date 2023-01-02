08eed6e42d0a33700967247ac7b66c7e92e23f99
Justin Hileman <justin@justinhileman.info>
2023-01-02 12:59:37 -0500

    $year++

--- test/Exception/ParseErrorExceptionTest.php
+++ test/Exception/ParseErrorExceptionTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Psy Shell.
  *
- * (c) 2012-2022 Justin Hileman
+ * (c) 2012-2023 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
