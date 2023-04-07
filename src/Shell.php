92c6f7509135e2f2029a5ef5bc3b3b65b03d3445
Justin Hileman <justin@justinhileman.info>
2023-04-08 07:56:59 +1000

    Bump to v0.11.15

--- src/Shell.php
+++ src/Shell.php
@@ -49,7 +49,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.11.14';
+    const VERSION = 'v0.11.15';
 
     /** @deprecated */
     const PROMPT = '>>> ';
