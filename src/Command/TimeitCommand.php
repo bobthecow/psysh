2f8c55565e024207136e87eeb70f49fe269c783a
Justin Hileman <justin@justinhileman.info>
2023-04-27 10:30:00 -0400

    Store timeit times as nanoseconds, round after all the math.

--- src/Command/TimeitCommand.php
+++ src/Command/TimeitCommand.php
@@ -28,6 +28,7 @@ class TimeitCommand extends Command
     const RESULT_MSG = '<info>Command took %.6f seconds to complete.</info>';
     const AVG_RESULT_MSG = '<info>Command took %.6f seconds on average (%.6f median; %.6f total) to complete.</info>';
 
+    // All times stored as nanoseconds!
     private static $useHrtime;
     private static $start = null;
     private static $times = [];
@@ -104,13 +105,13 @@ HELP
         self::$times = [];
 
         if ($num === 1) {
-            $output->writeln(\sprintf(self::RESULT_MSG, $times[0]));
+            $output->writeln(\sprintf(self::RESULT_MSG, $times[0] / 1e+9));
         } else {
             $total = \array_sum($times);
             \rsort($times);
             $median = $times[\round($num / 2)];
 
-            $output->writeln(\sprintf(self::AVG_RESULT_MSG, $total / $num, $median, $total));
+            $output->writeln(\sprintf(self::AVG_RESULT_MSG, ($total / $num) / 1e+9, $median / 1e+9, $total / 1e+9));
         }
 
         return 0;
@@ -125,7 +126,7 @@ HELP
      */
     public static function markStart()
     {
-        self::$start = self::$useHrtime ? (\hrtime(true) / 1e+9) : \microtime(true);
+        self::$start = self::$useHrtime ? \hrtime(true) : (\microtime(true) * 1e+6);
     }
 
     /**
@@ -144,7 +145,7 @@ HELP
      */
     public static function markEnd($ret = null)
     {
-        self::$times[] = (self::$useHrtime ? (\hrtime(true) / 1e+9) : \microtime(true)) - self::$start;
+        self::$times[] = (self::$useHrtime ? \hrtime(true) : (\microtime(true) * 1e+6)) - self::$start;
         self::$start = null;
 
         return $ret;
