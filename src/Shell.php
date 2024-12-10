2be491aa60394c6890564bd77d010991e67f2abb
Justin Hileman <justin@justinhileman.info>
2024-12-09 20:58:26 -0500

    Bump to v0.12.7

--- src/Shell.php
+++ src/Shell.php
@@ -53,7 +53,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.12.6';
+    const VERSION = 'v0.12.7';
 
     private Configuration $config;
     private CodeCleaner $cleaner;
