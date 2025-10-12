0e60527cb040b90a1aaeb6e150511ce61b606613
Justin Hileman <justin@justinhileman.info>
2025-10-12 13:35:39 -0400

    A bit late on this one.

--- src/Exception/ParseErrorException.php
+++ src/Exception/ParseErrorException.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Psy Shell.
  *
- * (c) 2012-2023 Justin Hileman
+ * (c) 2012-2025 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- test/Formatter/CodeFormatterTest.php
+++ test/Formatter/CodeFormatterTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Psy Shell.
  *
- * (c) 2012-2023 Justin Hileman
+ * (c) 2012-2025 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -138,7 +138,7 @@ EOS;
 /*
  * This file is part of Psy Shell.
  *
- * (c) 2012-2023 Justin Hileman
+ * (c) 2012-2025 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -171,7 +171,7 @@ EOS;
  3: /*
  4:  * This file is part of Psy Shell.
  5:  *
- 6:  * (c) 2012-2023 Justin Hileman
+ 6:  * (c) 2012-2025 Justin Hileman
  7:  *
  8:  * For the full copyright and license information, please view the LICENSE
  9:  * file that was distributed with this source code.
