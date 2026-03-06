d5ebbe4206ab7516c48e32183e7baccf1393d68a
Justin Hileman <justin@justinhileman.info>
2026-03-06 16:21:28 -0500

    Bump to v0.12.21

--- src/Shell.php
+++ src/Shell.php
@@ -65,7 +65,7 @@ use Symfony\Component\Console\Output\StreamOutput;
  */
 class Shell extends Application
 {
-    const VERSION = 'v0.12.20';
+    const VERSION = 'v0.12.21';
 
     private Configuration $config;
     private ?CodeCleaner $cleaner = null;
